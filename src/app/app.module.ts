b9c034c846eaa7a0453ccb0c56a007df72add8cb
Smith <allysmith424@gmail.com>
2018-08-11 13:49:21 -0500

    add component files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { SearchSuggestionsComponent } from './search-suggestions/search-suggestions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBoxComponent,
+    SearchResultsComponent,
+    SearchSuggestionsComponent
   ],
   imports: [
     BrowserModule
